54b79fdc4462133da9b7c0964d19a945b2a6f0bd
TurtleFeeder <t.choong@yahoo.com>
2018-12-01 11:57:20 -0500

    corrected syntax required for json-server to be able to read our db.json file

--- frontend/night-at-the-movies-client/src/containers/MoviesContainer.js
+++ frontend/night-at-the-movies-client/src/containers/MoviesContainer.js
@@ -2,7 +2,22 @@ import React, {Component} from 'react';
 import ViewStats from '../components/ViewStats';
 import GameContainer from './GameContainer';
 
+const MOVIE_URL = 'http://localhost:4000/movies'
+
 class MoviesContainer extends Component {
+  state = {
+    Movies: []
+  }
+
+  componentDidMount(){
+    // this.fetchAllMovies
+  }
+
+  fetchAllMovies = () => {
+
+  }
+
+
   render() {
     return (
       <div className="Movies-Container">
