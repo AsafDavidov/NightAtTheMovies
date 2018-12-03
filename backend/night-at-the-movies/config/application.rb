8a855501c75b9d5a13d751b6f65960e674cd6620
AsafDavidov <adavidov@conncoll.edu>
2018-12-03 10:50:21 -0500

    Completed Seed File for MovieQuote Table

--- backend/night-at-the-movies/config/application.rb
+++ backend/night-at-the-movies/config/application.rb
@@ -10,12 +10,20 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+require 'movie_quotes'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+HOSTNAME = ENV['API_KEY']
+MovieQuotes.configure do |config|
+  config.api_key = ENV['API_KEY']
+end
+
+
 
 module NightAtTheMovies
   class Application < Rails::Application
--- backend/night-at-the-movies/db/seeds.rb
+++ backend/night-at-the-movies/db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Sample Ruby code for user authorization
+#
+# t.string "content"
+# t.integer "year"
+# t.integer "rating"
+# t.string "title"
+# t.string "character"
+# t.string "actor"
+# t.string "img_url"
+# t.string "vid_url"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
+require 'nokogiri'
+require 'open-uri'
+require 'rest-client'
+#####################################SEEDING CUSTOMIZED MOVIE QUOTE API#########################################
+def youtube_search(query_term)
+        query_term = query_term + " trailer"
+        query_term = query_term.split(" ").map do |word|
+          word.scan(/\w+/)[0]
+        end.join("%20")
+        url = "https://www.youtube.com/results?search_query=#{query_term}"
+        document = Nokogiri::HTML(RestClient.get(url))
+        first_hit = document.css("a").map {|a| a['href']}.select {|link| link.include?("watch")&& link.length == 20}.first
+        first_hit_url = "https://www.youtube.com/#{first_hit}"
+end
+
+i=1
+10.times do
+  filter = MovieQuotes.new
+  filter.by_page(i).results.each do |result|
+    MovieQuote.create("content":result["content"],"year":result["year"],"rating":result["rating"],"title":result["movie"]["title"].split(" ").map{|word|word.scan(/\w+/)[0]}.join(" "),"character":result["character"]["name"],"actor":result["actor"]["name"],"img_url":result["image_large_url"],"vid_url":youtube_search(result["movie"]["title"]))
+  end
+  i = i+1
+end
+
+#####################################SEEDING CUSTOMIZED MOVIE QUOTE API#########################################
