923f0a86fafbd2c293880d093cdcacbf622a3cd5
TurtleFeeder <t.choong@yahoo.com>
2018-12-04 15:52:12 -0500

    attempt alerts for answer results - not successful

--- frontend/night-at-the-movies-client/src/components/GameComponents/PopBar.js
+++ frontend/night-at-the-movies-client/src/components/GameComponents/PopBar.js
@@ -3,8 +3,8 @@ import React from 'react';
 function PopBar({score}) {
 
   return (
-    <div class="pop-slidecontainer">
-      <input type="range" min="1" max="100" disabled="true"value={!!score ? score:50} class="pop-slider" id="myRange" />
+    <div className="pop-slidecontainer">
+      <input type="range" min="1" max="100" disabled={true} value={!!score ? score:0} className="pop-slider" id="myRange" />
     </div>
   )
 }
