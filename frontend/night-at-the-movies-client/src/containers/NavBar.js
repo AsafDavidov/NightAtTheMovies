4d3bd8887928c6e5699b8d2eae7dba67827b5b76
DDarrow123 <ddarrowphl@gmail.com>
2018-12-05 15:51:44 -0500

    finished styling title and hints

--- frontend/night-at-the-movies-client/src/containers/NavBar.js
+++ frontend/night-at-the-movies-client/src/containers/NavBar.js
@@ -1,14 +1,16 @@
+import '../App.css';
 import React from 'react';
 import {Menu} from 'semantic-ui-react';
 import './NavBar.css';
-import { BrowserRouter as Router, Route, Link, Redirect, NavLink, withRouter } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link, Redirect, NavLink, withRouter } from "react-router-dom";
+
 
 function NavBar() {
   return (
     <Router>
       <React.Fragment>
-        <h1 className="NavBar-header">Welcome to Night @ The Movies!</h1>
-        <Menu>
+        <h1 style={{marginBottom: 30}} className="NavBar-header">Night @ The Movies!</h1>
+        <Menu style={{marginBottom: 30}}>
           <Menu.Item name="Home" as={NavLink} to='/home'></Menu.Item>
           <Menu.Item name="Stats" onClick={()=> console.log('clicked Stat')}></Menu.Item>
         </Menu>
