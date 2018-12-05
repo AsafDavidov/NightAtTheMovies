dadf5d4bc2f13d37b108454c4aafe417a883f045
TurtleFeeder <t.choong@yahoo.com>
2018-12-05 15:23:39 -0500

    fixed merged conflict in Timer.js file again

--- frontend/night-at-the-movies-client/src/components/GameComponents/Timer.js
+++ frontend/night-at-the-movies-client/src/components/GameComponents/Timer.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 function Timer({time,handleStopTime}) {
   return (
-<<<<<<< HEAD
-    <h1 >Current Time Taken: {time}</h1>
-=======
     <h1>Current Time Taken: {time}</h1>
->>>>>>> DEV_FE
   )
 }
 
