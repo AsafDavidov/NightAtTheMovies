7d07da699fb764bdf8504f3fe81b432d1bb17e67
TurtleFeeder <t.choong@yahoo.com>
2018-12-05 13:49:09 -0500

    attempt NavBar routing

--- frontend/night-at-the-movies-client/src/containers/NavBar.js
+++ frontend/night-at-the-movies-client/src/containers/NavBar.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import {Menu} from 'semantic-ui-react';
 import './NavBar.css';
-// https://react.semantic-ui.com/collections/menu/#types-basic
+import { BrowserRouter as Router, Route, Link, Redirect, NavLink, withRouter } from "react-router-dom";
+import {withRouter} from 'react-router-dom'
 
 function NavBar() {
   return (
-    <React.Fragment>
-    <h1 className="NavBar-header">Welcome to Night @ The Movies!</h1>
-    <Menu>
-      <Menu.Item name="Home" active={true} ></Menu.Item>
-      <Menu.Item name="Boop" active={false} ></Menu.Item>
-      <Menu.Item name="Gotcha!" active={false} ></Menu.Item>
-    </Menu>
-    </React.Fragment>
+    <Router>
+      <React.Fragment>
+        <h1 className="NavBar-header">Welcome to Night @ The Movies!</h1>
+        <Menu>
+          <Menu.Item name="Home" as={NavLink} to='/home'></Menu.Item>
+          <Menu.Item name="Stats" onClick={()=> console.log('clicked Stat')}></Menu.Item>
+        </Menu>
+      </React.Fragment>
+    </Router>
   )
 }
 
@@ -30,4 +32,4 @@ function NavBar() {
 // }
 
 
-export default NavBar;
+export default withRouter(NavBar);
